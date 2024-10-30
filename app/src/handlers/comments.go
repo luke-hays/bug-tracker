287ca91ebdd6466d196aa68e317ad594c8152d5e
Midival <lukehays32@gmail.com>
2024-10-29 21:16:10 -0400

    finish adding comment insertion

--- app/src/handlers/comments.go
+++ app/src/handlers/comments.go
@@ -3,8 +3,11 @@ package handlers
 import (
 	"app/src/db"
 	helpers "app/src/utils"
+	"context"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func CreateComment(w http.ResponseWriter, r *http.Request, dbContext *db.DatabaseContext) {
@@ -18,11 +21,32 @@ func CreateComment(w http.ResponseWriter, r *http.Request, dbContext *db.Databas
 	key := helpers.AccountIdKey("account_id")
 	accountId := r.Context().Value(key)
 
-	fmt.Println(accountId)
+	fmt.Printf("accountId - %d\n", accountId)
 
-	// bugId := helpers.ParseInt(r.FormValue("bug_id"))
-	// author := helpers.ParseInt(r.FormValue("author"))
-	// commentDate := time.Now()
-	// comment := r.FormValue("comment")
+	bugId := r.FormValue("bug_id")
+	author := accountId
+	commentDate := time.Now()
+	comment := r.FormValue("comment")
 
+	bugIdNum, convErr := strconv.Atoi(bugId)
+
+	if convErr != nil {
+		helpers.WriteAndLogHeaderStatus(w, http.StatusBadRequest, "Unable to parse bug id")
+		return
+	}
+
+	fmt.Printf("%d, %d, %s, %s\n", bugIdNum, author, commentDate, comment)
+
+	_, insertErr := dbContext.Connection.Query(
+		context.Background(),
+		"INSERT INTO Comments (bug_id, author, comment_date, comment) VALUES ($1, $2, $3, $4)",
+		bugIdNum, author, commentDate, comment,
+	)
+
+	if insertErr != nil {
+		helpers.WriteAndLogHeaderStatus(w, http.StatusInternalServerError, "Unable to insert new comment")
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
